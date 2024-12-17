21e61da9c441710e98a7f85b88fcb9cb1bfa628c
Kirsty <congokirsty@gmail.com>
2024-12-17 17:01:49 +0000

    added utils file

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+
+export const formatDate = (dateString) => {
+    return new Date(dateString).toLocaleString(en-GB, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    })
+}
